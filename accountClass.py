9ea03172695bb7fdb85caf8355866016301871a1
Amarendra Bandla <devnullbandla@outlook.com>
2024-08-08 15:53:54 +0530

    chore: Add Account class with deposit, withdraw, and balance methods

---
+++ accountClass.py
@@ -0,0 +1,37 @@
+
+class Account():
+    def __init__(self, name, balance, password):
+        self.name = name
+        self.balance = balance
+        self.password = password
+
+    def deposit(self, amountToDeposit, password):
+        if amountToDeposit < 0:
+            print('You cannot deposit a negative amount!')
+            return None
+        if password != self.password:
+            print('Incorrect password')
+            return None
+        self.balance = self.balance + amountToDeposit
+        return self.balance
+    def withdraw(self, amountToWithdraw, password):
+        if amountToWithdraw < 0:
+            print('You cannot withdraw a negative amount')
+            return None
+        if password != self.password:
+            print('Incorrect password for this account')
+            return None
+        if amountToWithdraw > self.balance:
+            print('You cannot withdraw more than you have in your account')
+            return None
+        self.balance = self.balance - amountToWithdraw
+        return self.balance
+    def getBalance(self, password):
+        if password != self.password:
+            print('Incorrect password')
+            return None
+        return self.balance
+    def show(self):
+        print('       Name', self.name)
+        print('       Balance:', self.balance)
+        print()
