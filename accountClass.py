4a0e66ba8ee85cf8653d19c1440938d926d18073
Amarendra Bandla <devnullbandla@outlook.com>
2024-08-08 16:48:39 +0530

    chore: Add switch class with methods to control state and brightness

--- accountClass.py
+++ accountClass.py
@@ -55,3 +55,38 @@ testObject2 = testClass(10,20,30)
 print(testObject2.p1)
 testObject2.increment(5)
 print(testObject2.p1)
+
+class switch():
+    def __init__(self):
+        self.state = 'off'
+        self.brightness = 0
+    
+    def turnOn(self):
+        self.state = 'on'
+        return self.state
+    
+    def raiseBrightness(self, brightness):
+        if brightness + self.brightness > 100:
+            print('Brightness cannot exceed 100')
+            return None
+        self.brightness = self.brightness + brightness
+        return self.brightness
+    
+    def lowerBrightness(self, brightness):
+        if self.brightness - brightness < 0:
+            print ('Brightness cannot be less than 0')
+            return None
+        self.brightness = self.brightness - brightness
+        return self.brightness
+    
+    def show(self):
+        print('State:', self.state)
+        print('Brightness:', self.brightness)
+        print()
+
+switchObject = switch()
+print(switchObject.state)
+switchObject.turnOn()
+switchObject.raiseBrightness(10)
+print(switchObject.state)
+print(switchObject.brightness)
