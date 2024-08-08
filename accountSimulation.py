3d6213621b8d523657217ee0b5b9285b345e7390
Amarendra Bandla <devnullbandla@outlook.com>
2024-08-08 15:44:37 +0530

    updated script.

--- accountSimulation.py
+++ accountSimulation.py
@@ -1,6 +1,58 @@
-accountName = 'Joe'
-accountBalance = 100
-accountPassword = 'soup'
+accountName = ''
+accountBalance = 0
+accountPassword = ''
+
+def newAccount(name, balance, password):
+    global accountName, accountBalance, accountPassword
+    accountName = name
+    accountBalance = balance
+    accountPassword = password
+
+def show():
+    global accountName, accountBalance, accountPassword
+    print('       Name', accountName)
+    print('       Balance:', accountBalance)
+    print('       Password:', accountPassword)
+    print()
+
+def getBalance(password):
+    global accountName, accountBalance, accountPassword
+    if password != accountPassword:
+        print('Incorrect password')
+        return None
+    return accountBalance
+
+def deposit(amountToDeposit, password):
+    global accountName, accountBalance, accountPassword
+    if amountToDeposit < 0:
+        print('You cannot deposit a negative amount!')
+        return None
+        
+    if password != accountPassword:
+        print('Incorrect password')
+        return None
+
+    accountBalance = accountBalance + amountToDeposit
+    return accountBalance
+
+def withdraw(amountToWithdraw, password):
+    global accountName, accountBalance, accountPassword
+    if amountToWithdraw < 0:
+        print('You cannot withdraw a negative amount')
+        return None
+
+    if password != accountPassword:
+        print('Incorrect password for this account')
+        return None
+
+    if amountToWithdraw > accountBalance:
+        print('You cannot withdraw more than you have in your account')
+        return None
+
+    accountBalance = accountBalance - amountToWithdraw
+    return accountBalance
+
+newAccount("Joe", 100, 'soup')  # create an account
 
 while True:
     print()
@@ -15,14 +67,13 @@ while True:
     action = action.lower()  # force lowercase
     action = action[0]  # just use first letter
     print()
-
+    
     if action == 'b':
         print('Get Balance:')
         userPassword = input('Please enter the password: ')
-        if userPassword != accountPassword:
-            print('Incorrect password')
-        else:
-            print('Your balance is:', accountBalance)
+        theBalance = getBalance(userPassword)
+        if theBalance is not None:
+            print('Your balance is:', theBalance)
 
     elif action == 'd':
         print('Deposit:')
@@ -30,44 +81,8 @@ while True:
         userDepositAmount = int(userDepositAmount)
         userPassword = input('Please enter the password: ')
 
-        if userDepositAmount < 0:
-            print('You cannot deposit a negative amount!')
-
-        elif userPassword != accountPassword:
-            print('Incorrect password')
-
-        else:  # OK
-            accountBalance = accountBalance + userDepositAmount
-            print('Your new balance is:', accountBalance)
-        
-    elif action == 's':  # show
-        print('Show:')
-        print('       Name', accountName)
-        print('       Balance:', accountBalance)
-        print('       Password:', accountPassword)
-        print()
-
-    elif action == 'q':
-        break
-
-    elif action == 'w':
-        print('Withdraw:')
-
-        userWithdrawAmount = input('Please enter the amount to withdraw: ')
-        userWithdrawAmount = int(userWithdrawAmount)
-        userPassword = input('Please enter the password: ')
-
-        if userWithdrawAmount < 0:
-            print('You cannot withdraw a negative amount')
-
-        elif userPassword != accountPassword:
-            print('Incorrect password for this account')
-
-        elif userWithdrawAmount > accountBalance:
-            print('You cannot withdraw more than you have in your account')
-
-        else:  #OK
-            accountBalance = accountBalance - userWithdrawAmount
-            print('Your new balance is:', accountBalance)
+        newBalance = deposit(userDepositAmount, userPassword)
+        if newBalance is not None:
+            print('Your new balance is:', newBalance)   
 
 print('Done')
